069e2db8e7bc5f821c4f1d73011a5ced01f7c9e7
Samuel K <ashersamuel8@gmail.com>
2021-09-21 12:09:46 -0400

    updated CollectionManager and Album Classes

--- src/Collection.java
+++ src/Collection.java
@@ -1,14 +1,17 @@
 
 public class Collection {
-
-	private String title;
-	private String artist;
-	private Genre genre; //enum class; Classical, Country, Jazz, Pop, Unknown private Date releaseDate;
-	private boolean isAvailable; ...
-	@Override
-	public boolean equals(Object obj) { }
-	...
-	@Override
-	public String toString() { }
 	
+	private Album[] albums;
+	private int numAlbums; //number of albums currently in the collection
+	
+	private int find(Album album) {} //find the album index, or return NOT_FOUND 
+	private void grow() {} //increase the capacity of the array list by 4 
+	public boolean add(Album album) {}
+	public boolean remove(Album album) {}
+	public boolean lendingOut(Album album) {} //set to not available 
+	public boolean returnAlbum(Album album) {} //set to available
+	public void print() {} //display the list without specifying the order 
+	public void printByReleaseDate() {}
+	public void printByGenre() {}
+
 }
